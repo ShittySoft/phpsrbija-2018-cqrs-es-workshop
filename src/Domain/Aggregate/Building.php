cec1e23c19ad536f031e3856ebac412bd9025b56
Marco Pivetta <ocramius@gmail.com>
2018-05-25 12:52:05 +0200

    Check-in functionality

--- src/Domain/Aggregate/Building.php
+++ src/Domain/Aggregate/Building.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Building\Domain\Aggregate;
 
 use Building\Domain\DomainEvent\NewBuildingWasRegistered;
+use Building\Domain\DomainEvent\UserCheckedIn;
 use Prooph\EventSourcing\AggregateRoot;
 use Rhumsaa\Uuid\Uuid;
 
@@ -34,20 +35,28 @@ final class Building extends AggregateRoot
         return $self;
     }
 
-    public function checkInUser(string $username)
+    public function checkInUser(string $username) : void
+    {
+        $this->recordThat(UserCheckedIn::toBuilding(
+            $this->uuid,
+            $username
+        ));
+    }
+
+    public function checkOutUser(string $username) : void
     {
         // @TODO to be implemented
     }
 
-    public function checkOutUser(string $username)
-    {
-        // @TODO to be implemented
-    }
-
-    public function whenNewBuildingWasRegistered(NewBuildingWasRegistered $event)
+    protected function whenNewBuildingWasRegistered(NewBuildingWasRegistered $event) : void
     {
         $this->uuid = Uuid::fromString($event->aggregateId());
         $this->name = $event->name();
+    }
+
+    protected function whenUserCheckedIn(UserCheckedIn $event) : void
+    {
+        // Nothing for now
     }
 
     /**
